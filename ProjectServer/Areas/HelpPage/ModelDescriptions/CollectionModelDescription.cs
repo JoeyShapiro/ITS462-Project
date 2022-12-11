95009ce3c09fd64335b49012df2e2b21b023ce52
JoeyShapiro <57827758+JoeyShapiro@users.noreply.github.com>
2022-12-11 10:30:19 -0800

    removed old server

--- ProjectServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjectServer.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ProjectServer/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProjectServer.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
